8ea55dd95b806fcc935dcf1d50497d9227f38f21
Nerus-Zaumu <neruszaumuy@gmail.com>
2023-03-02 16:48:18 +0100

    Initial Commit

---
+++ mobile-store/src/main.ts
@@ -0,0 +1,22 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+
+import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { IonicRouteStrategy, IonicModule } from '@ionic/angular';
+import { RouteReuseStrategy } from '@angular/router';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, IonicModule.forRoot(), AppRoutingModule),
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ]
+})
+  .catch(err => console.log(err));
